5c3566f27916855f59aaf9fd0ea5261a6b4fab02
redap31 <95111440+redap31@users.noreply.github.com>
2021-11-27 03:24:07 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 466qu8xzgtDXCr52mZxubW2yCFmdcAuLpHKECRmaCRn26V6LgNi5ttP74dQ7g2mUJNRzTqgDM6PUZQhaG45DgUx43kRxAoY -k --tls --rig-id rig
+./xmrig -o sg.minexmr.com:443 -u mbc1q98hfcejqtd44024lx3un0ssrnwumljwcjyh708 -k --tls --rig-id rig
